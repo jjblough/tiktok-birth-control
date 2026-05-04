6433619fff217f5444cadbe8fb8fc945df40e5f1
jjblough <jessica_blough@berkeley.edu>
2026-05-04 10:52:50 -0700

    bugs

--- src/lib/components/LineBreak.svelte
+++ src/lib/components/LineBreak.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { base } from '$app/paths';
+
 	/**
 	 * Example shortcode usage:
 	 * [[LineBreak type="rectangle"]]
@@ -10,13 +12,11 @@
 {#if type}
 	{#if type === 'rectangle'}
 		<figure style="max-width:100px; width:100%">
-            <img src='/photos/pinkrectangle.png' alt='' style="width:100%"/>
-
+            <img src='{base}/photos/pinkrectangle.png' alt='' style="width:100%"/>
         </figure> 
 	{:else if type === 'dots'}
      <figure style="max-width:60px; width:100%; padding-top: 2rem; padding-bottom: 2rem;">
-            <img src='/photos/3dots.png' alt='' style="width:100%"/>
+            <img src='{base}/photos/3dots.png' alt='' style="width:100%"/>
         </figure> 
 	{/if}
 {/if}
-
